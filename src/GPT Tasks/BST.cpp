5b3adb2957565811bc19fded77433b86b72d3e1f
DfDffeWin <154010501+DfDffeWin@users.noreply.github.com>
2024-06-07 09:27:07 +0200

    completed new task

---
+++ src/GPT Tasks/BST.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+
+class TreeNode {
+public:
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int value) : data(value), left(nullptr), right(nullptr) {}
+};
+
+class BinarySearchTree {
+private:
+    TreeNode* root;
+
+    TreeNode* insertNode(TreeNode* root, int value) {
+        if (root == nullptr) {
+            return new TreeNode(value);
+        }
+
+        if (value < root->data) {
+            root->left = insertNode(root->left, value);
+        } else if (value > root->data) {
+            root->right = insertNode(root->right, value);
+        }
+
+        return root;
+    }
+
+    TreeNode* findMinNode(TreeNode* node) {
+        while (node->left != nullptr) {
+            node = node->left;
+        }
+        return node;
+    }
+
+    TreeNode* deleteNode(TreeNode* root, int value) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+
+        if (value < root->data) {
+            root->left = deleteNode(root->left, value);
+        } else if (value > root->data) {
+            root->right = deleteNode(root->right, value);
+        } else {
+            if (root->left == nullptr) {
+                TreeNode* temp = root->right;
+                delete root;
+                return temp;
+            } else if (root->right == nullptr) {
+                TreeNode* temp = root->left;
+                delete root;
+                return temp;
+            }
+
+            TreeNode* minRight = findMinNode(root->right);
+            root->data = minRight->data;
+            root->right = deleteNode(root->right, minRight->data);
+        }
+
+        return root;
+    }
+
+    void inOrderTraversal(TreeNode* root) {
+        if (root != nullptr) {
+            inOrderTraversal(root->left);
+            std::cout << root->data << " ";
+            inOrderTraversal(root->right);
+        }
+    }
+
+public:
+    BinarySearchTree() : root(nullptr) {}
+
+    void insert(int value) {
+        root = insertNode(root, value);
+    }
+
+    void remove(int value) {
+        root = deleteNode(root, value);
+    }
+
+    void printInOrder() {
+        std::cout << "In-order traversal: ";
+        inOrderTraversal(root);
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    BinarySearchTree bst;
+
+    // Insert some values
+    bst.insert(50);
+    bst.insert(30);
+    bst.insert(70);
+    bst.insert(20);
+    bst.insert(40);
+    bst.insert(60);
+    bst.insert(80);
+
+    // Print in-order traversal
+    bst.printInOrder();
+
+    // Remove a value
+    bst.remove(30);
+
+    // Print in-order traversal after removal
+    bst.printInOrder();
+
+    return 0;
+}
